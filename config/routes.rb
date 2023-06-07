802cabbf0407560d016ddd7511c1ff8e4034c62d
ArshanBhanage <bhanagearshan@gmail.com>
2023-06-07 11:03:39 +0000

    pushing rails api

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+      
+    namespace :api do
+      namespace :v1 do
+        defaults format: :json do
+          get 'home/index', to: 'home#index'
+        end
+      end
+    end
+      
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
